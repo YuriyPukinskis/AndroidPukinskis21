bde9f2a729251ed87520d3d46e757f1b7bbc2f03
Pukinskis <pukinskis@mail.ru>
2019-05-11 21:15:09 +0300

    правка

--- app/src/main/java/ru/netology/androidpukinskis21/MainActivity.java
+++ app/src/main/java/ru/netology/androidpukinskis21/MainActivity.java
@@ -16,12 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        String str ="";
         Button btnClear = (Button) findViewById(R.id.buttonClear);
+        btnClear.setText(getString (R.string.s3));
         Button btnOk = (Button) findViewById(R.id.buttonOk);
+        btnOk.setText(getString (R.string.s4));
         final EditText editText = (EditText)findViewById(R.id.name);
+        editText.setHint(getString (R.string.s5));
         final EditText mail = (EditText)findViewById(R.id.mail);
-        // TextInputEditText tMail = (TextInputEditText) findViewById(R.id.mail);
+        mail.setHint(getString (R.string.s6));
         final TextView textView = findViewById(R.id.textView);
 
        btnClear.setOnClickListener(new View.OnClickListener(){
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
        btnOk.setOnClickListener(new View.OnClickListener(){
            @Override
            public void onClick(View v) {
-               textView.setText("Подписка на рассылку успешно оформлена для пользователя " + editText.getText() + " на 'электронный адрес " +mail.getText() + ".");
+               textView.setText(getString (R.string.s1) + editText.getText() + getString (R.string.s2) +mail.getText() + ".");
            }
        });
     }
